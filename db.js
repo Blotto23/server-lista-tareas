0ad91afea3e6320483f5d009002bb1ea982e97fd
Jorge Rivera <jlriveraya@gmail.com>
2023-06-29 14:01:25 -0500

    mongodb

---
+++ db.js
@@ -0,0 +1,21 @@
+const { MongoClient } = require(mongodb);
+
+const env = require("dotenv");
+
+env.config();
+
+const dbName = "tasklist";
+const client = new MongoClient(process.env.MONGO_URI);
+
+const connect = async () => {
+    try {
+        await client.connect();
+    } catch (error) {
+        console.log(error);
+    }
+    return client.db(dbName);
+}
+
+const db = connect();
+
+module.exports = db;
